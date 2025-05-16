5b70331bd9d80fbe53bb60ff3f9aef9342f91689
Daren Schaad <darenschaad@gmail.com>
2025-05-15 20:25:55 -0700

    added base.html and some other templates to the site

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Christia'},
+            'body': 'The only thing left in this world worth living for is Fallout!'
+        },
+        {
+            'author': {'username': 'Daren'},
+            'body': 'Why am I so grumpy all the time?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
