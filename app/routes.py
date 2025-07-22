f5296e1a342efa588aeb110a7211761216e9dde9
Daren Schaad <darenschaad@gmail.com>
2025-07-21 20:33:22 -0700

    added the @login_required decorator to /index route

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -40,3 +41,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
