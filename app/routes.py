2e4951841cfc49e3e7939ff8c0564ca7e639e1cc
Daren Schaad <darenschaad@gmail.com>
2025-07-21 21:14:27 -0700

    added logic for the next url during the login process

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
+from urllib.parse import urlsplit
 
 @app.route('/')
 @app.route('/index')
@@ -39,8 +40,11 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    #return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
