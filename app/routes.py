3f0e50357876b29ce33c41bff57eb72007520b02
Daren Schaad <darenschaad@gmail.com>
2025-08-13 18:17:28 -0700

    set the last seen time in routes.py in the @before_request decorator

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,13 @@ from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import db
 from app.models import User
+from datetime import datetime, timezone
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
